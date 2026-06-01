ebccd66718facaca530a49ae742f76f9511d19ea
Shaktiz <shaktisinhzala8900@gmail.com>
2026-06-01 11:01:35 +0530

    fixed created_at in posts

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .database import Base
-from sqlalchemy import TIME,Column, ForeignKey,Integer,String,Boolean,text
+from sqlalchemy import TIME, TIMESTAMP,Column, ForeignKey,Integer,String,Boolean,text
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 class Post(Base):
     __tablename__='posts'
@@ -9,7 +10,7 @@ class Post(Base):
     title=Column(String,nullable=False)
     content=Column(String,nullable=False)
     published=Column(Boolean,server_default="True",nullable=False)
-    created_at=Column(TIME(timezone=True),nullable=False,server_default=text("now()"))
+    created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text("now()"))
     owner_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
     
     owner=relationship("Users")
