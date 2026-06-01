1b33eda664b084e832b05bb5b352f0818f2ea3ab
Shaktiz <shaktisinhzala8900@gmail.com>
2026-06-01 11:09:47 +0530

    added datetime in posts

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from .database import Base
-from sqlalchemy import TIME, TIMESTAMP,Column, ForeignKey,Integer,String,Boolean,text
+from sqlalchemy import TIME, TIMESTAMP,Column, DateTime, ForeignKey,Integer,String,Boolean, func,text
 from sqlalchemy.orm import relationship
-from datetime import datetime
+# from datetime import datetime
 
 class Post(Base):
     __tablename__='posts'
@@ -10,7 +10,8 @@ class Post(Base):
     title=Column(String,nullable=False)
     content=Column(String,nullable=False)
     published=Column(Boolean,server_default="True",nullable=False)
-    created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text("now()"))
+    # created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text("now()"))
+    created_at=Column(DateTime(timezone=True),server_default=func.now(),nullable=False)
     owner_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
     
     owner=relationship("Users")
