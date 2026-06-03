42a1710875920033381647d05a9a41412a543b91
Shaktiz <shaktisinhzala8900@gmail.com>
2026-06-03 14:25:14 +0530

    fixed reuirements

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .routers import user,posts,auth,vote
 from app.config import settings
 from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
