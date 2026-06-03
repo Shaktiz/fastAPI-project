726bb7e8ab000f92d6f87d32b003ef7c112a5f68
Shaktiz <shaktisinhzala8900@gmail.com>
2026-06-03 19:01:04 +0530

    updated search toggle

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -20,13 +20,54 @@
 
 // export default Dashboard;
 
+// import Navbar from "../components/Navbar";
+// import Posts from "../components/Posts";
+// import Users from "../components/Users";
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// function Dashboard() {
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("token");
+
+//     if (!token) {
+//       alert("Please login first");
+//       navigate("/");
+//     }
+//   }, [navigate]);
+
+//   return (
+//     <>
+//       <Navbar />
+
+//       <div className="container mt-4">
+//         <div className="row">
+//           <div className="col-md-8">
+//             <Posts />
+//           </div>
+
+//           <div className="col-md-4">
+//             <Users />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Dashboard;
+
+
+
 import Navbar from "../components/Navbar";
 import Posts from "../components/Posts";
 import Users from "../components/Users";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Dashboard() {
+function Dashboard({ search, setSearch }) {
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -40,14 +81,19 @@ function Dashboard() {
 
   return (
     <>
-      <Navbar />
+      <Navbar
+        search={search}
+        setSearch={setSearch}
+      />
 
       <div className="container mt-4">
         <div className="row">
+          {/* Posts Section */}
           <div className="col-md-8">
-            <Posts />
+            <Posts search={search} />
           </div>
 
+          {/* Users Section */}
           <div className="col-md-4">
             <Users />
           </div>
